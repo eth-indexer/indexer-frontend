f055b37288137e2052ab197b9dc7721c9d4ec3d0
kolsolv <kolsolv@gmail.com>
2024-12-18 05:30:31 +0400

    Fix keys length calculation

--- src/components/StateView/StateView.tsx
+++ src/components/StateView/StateView.tsx
@@ -23,7 +23,7 @@ const StateView: FC<StateViewProps> = ({ blockWithNonce }) => {
   });
 
   const getKeysInfo = (keys: string) => {
-    return keys.slice(2, keys.length).match(/.{1,96}/g)?.length;
+    return keys.replace(/0x/g, "").match(/.{1,96}/g)?.length;
   };
 
   const parseBigIntFromString = (bigIntString: string) => {
